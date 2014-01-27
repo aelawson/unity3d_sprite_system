fe3bbbe9072abd1a86e7dd42769dc502dff9e993
Andrew Lawson <alawson@engr.uconn.edu>
2014-01-27 09:49:35 -0500

    Initial commit.

---
+++ SpritesheetAnimator.cs
@@ -0,0 +1,127 @@
+﻿// author: Andrew Lawson
+// date: 10.25.13 
+
+// !USAGE!
+// Animates a spritesheet from a given image and uv map from the user. User can control time in-between
+// sprite frames.
+
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Globalization;
+
+public class SpritesheetAnimator : MonoBehaviour {
+	
+	// Private Instance Variables
+	
+	private Mesh
+		objectMesh;
+	private Rect[]
+		textureUV;
+	private Vector2[]
+		originalUV;
+	private int
+		counter = 0;
+	
+	// Public Instance Variables
+	
+	public Material
+		material;
+	public TextAsset
+		uvMap;
+	public float
+		framesPerSecond;
+	public bool
+		pause = false;
+	
+	void Start() {
+		
+		// Intitializations
+		
+		objectMesh = GetComponent<MeshFilter>().mesh;
+		originalUV = new Vector2[objectMesh.uv.Length];
+		objectMesh.uv.CopyTo(originalUV, 0);
+		
+		// Read uv maps from text file
+		
+		textureUV = readUVMap();
+		gameObject.renderer.material = material;
+		
+		StartCoroutine("CoUpdate");
+	}
+	
+	// Coroutine that runs the frame animation.
+	
+	IEnumerator FrameAnimation() {
+		
+		// Reset the UV map
+		
+		Vector2[] transformUV = new Vector2[originalUV.Length];
+		
+		// For each texture in the atlas, transform the mesh UV map to the map of the texture.
+
+		originalUV.CopyTo(transformUV, 0);
+			
+		for (int j = 0; j < objectMesh.uv.Length; j++) {
+				
+			transformUV[j].x = (transformUV[j].x * textureUV[counter].width) + textureUV[counter].x;
+			transformUV[j].y = (transformUV[j].y * textureUV[counter].height) + textureUV[counter].y;
+		}
+		
+		// Increase counter and set the object's mesh to the transformation.
+		
+		
+		if (!pause) {
+			
+			counter++;
+		}
+		
+		
+		objectMesh.uv = transformUV;
+		
+		yield return null;
+	}
+	
+	// Replaces Update() to allow for proper co-routine execution at runtime. Starts the animation co-routine.
+	
+	IEnumerator CoUpdate() {
+		
+		while (true) {
+		
+			if (counter >= textureUV.Length) {
+				
+				counter = 0;
+			}
+			
+			StartCoroutine("FrameAnimation");
+			
+			// Allows control of time in-between frame animations.
+			
+			yield return new WaitForSeconds((float)(1 / framesPerSecond));
+		}
+	}
+	
+	Rect[] readUVMap() {
+		
+		// Intializes a reader and retrieves length of UV entries
+		
+		StringReader reader = new StringReader(uvMap.text);
+		int length = int.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+		
+		Rect[] uvArray = new Rect[length];
+		
+		// Stores entries from text file into array and returns it
+		
+		for (int i = 0; i < length; i++) {
+			
+			float x = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+			float y = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+			float width = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+			float height = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+			
+			uvArray[i] = new Rect(x, y, width, height);
+		}
+		
+		return uvArray;
+	}
+} 
---
+++ SpritesheetGenerator.cs
@@ -0,0 +1,134 @@
+﻿// author: Andrew Lawson
+// date: 10.25.13 
+
+// !USAGE!
+// Generates a spritesheet and uv map from a set of given images.
+
+using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Globalization;
+
+public class SpritesheetGenerator : MonoBehaviour {
+	
+	// Private Instance Variables
+	
+	private Texture2D[]
+		textureArray;
+	private Mesh
+		objectMesh;
+	private Rect[]
+		uvArray,
+		textureUV;
+	private Vector2[]
+		originalUV;
+	
+	// Public Instance Variables
+	
+	public Texture2D
+		spriteTexture;
+	public string
+		filePath;
+	public string
+		spriteName,
+		destDirPath;
+	public Material
+		spriteMaterial;
+	
+	void Start() {
+		
+		// Load all source images from 'Resources/"filePath"'
+		
+		Object[] resourceArray = Resources.LoadAll(filePath);		
+		textureArray = new Texture2D[resourceArray.Length];
+		
+		for (int i = 0; i < resourceArray.Length; i++) {
+			
+			textureArray[i] = (Texture2D) resourceArray[i];
+		}
+		
+		resourceArray = null;
+		
+		// Instantiate a variable holding the UV map of the mesh.
+		
+		objectMesh = GetComponent<MeshFilter>().mesh;
+		originalUV = new Vector2[objectMesh.uv.Length];
+		objectMesh.uv.CopyTo(originalUV, 0);
+		
+		// Pack individual textures into a texture atlas.
+		
+		float maxSqrt = Mathf.Ceil(Mathf.Sqrt(textureArray[0].width * textureArray[0].height * textureArray.Length));
+			
+		if (maxSqrt > 8192) {
+			
+			maxSqrt = 8192;
+		}
+		
+		// Create sprite sheet
+			
+		Texture2D textureAtlas = new Texture2D((int)maxSqrt,
+			(int)maxSqrt);
+		
+		textureUV = textureAtlas.PackTextures(textureArray, 0, 
+			(int)maxSqrt);
+
+		// Write PNG spritesheet to file
+		
+		System.IO.Directory.CreateDirectory(Application.dataPath + "/Resources/" + destDirPath);
+		
+		byte[] pngBytes = textureAtlas.EncodeToPNG();
+		File.WriteAllBytes(Application.dataPath + "/Resources/" + destDirPath + spriteName + "_sheet" + ".png", pngBytes);
+		
+		//spriteMaterial = new Material(Shader.Find ("Transparent/Diffuse"));
+		//spriteTexture = (Texture2D) Resources.Load(destDirPath + spriteName + "_sheet");
+		//spriteMaterial.SetTexture("_MainTex", spriteTexture);
+		//renderer.material = spriteMaterial;
+		
+		// Output uv map to text file
+		
+		createUVMap();
+	}
+
+	void Update() {
+		
+		// nothing
+	}
+	
+	void createUVMap() { 
+		
+		// Intialize output writer
+		
+		StreamWriter writer; 
+		FileInfo t = new FileInfo(Application.dataPath + "/Resources/" + destDirPath + spriteName + ".txt"); 
+		
+		if (!t.Exists) {
+		
+		 	writer = t.CreateText(); 
+		}
+	
+		else {
+		
+			t.Delete(); 
+			writer = t.CreateText(); 
+		} 
+		
+		writer.Write(textureUV.Length);
+		writer.WriteLine();
+		
+		// Write uv map from textureUV to text file.
+		
+		for (int i = 0; i < textureUV.Length; i++) {
+			
+			writer.Write(textureUV[i].x);
+			writer.WriteLine();
+			writer.Write(textureUV[i].y);
+			writer.WriteLine();
+			writer.Write(textureUV[i].width);
+			writer.WriteLine();
+			writer.Write(textureUV[i].height);
+			writer.WriteLine();
+		} 
+		
+		writer.Close(); 
+	} 
+} 
