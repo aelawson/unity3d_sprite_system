48d87a23d16ed706ea71b4c1f01f33d8749a5bb7
Andrew Lawson <alawson@engr.uconn.edu>
2014-07-20 19:50:26 -0400

    Update SpritesheetAnimator.cs
    Updated comments and whitespace.

--- SpritesheetAnimator.cs
+++ SpritesheetAnimator.cs
@@ -11,117 +11,69 @@ using System.IO;
 using System.Globalization;
 
 public class SpritesheetAnimator : MonoBehaviour {
-	
-	// Private Instance Variables
-	
-	private Mesh
-		objectMesh;
-	private Rect[]
-		textureUV;
-	private Vector2[]
-		originalUV;
-	private int
-		counter = 0;
-	
-	// Public Instance Variables
-	
-	public Material
-		material;
-	public TextAsset
-		uvMap;
-	public float
-		framesPerSecond;
-	public bool
-		pause = false;
-	
+	private Mesh objectMesh;
+	private Rect[] textureUV;
+	private Vector2[] originalUV;
+	private int counter = 0;
+	public Material material;
+	public TextAsset uvMap;
+	public float framesPerSecond;
+	public bool pause = false;
+	// Initialization code...
 	void Start() {
-		
-		// Intitializations
-		
+		// Retrieve mesh from active object and copy its original UV map
 		objectMesh = GetComponent<MeshFilter>().mesh;
 		originalUV = new Vector2[objectMesh.uv.Length];
 		objectMesh.uv.CopyTo(originalUV, 0);
-		
-		// Read uv maps from text file
-		
+		// Read the UV maps from file
 		textureUV = readUVMap();
 		gameObject.renderer.material = material;
-		
+		// Start the update (loop) co-routine
 		StartCoroutine("CoUpdate");
 	}
-	
-	// Coroutine that runs the frame animation.
-	
+	// Co-routine that runs the frame-by-frame sprite animation.
 	IEnumerator FrameAnimation() {
-		
 		// Reset the UV map
-		
 		Vector2[] transformUV = new Vector2[originalUV.Length];
-		
-		// For each texture in the atlas, transform the mesh UV map to the map of the texture.
-
+		// For each texture in the atlas, transform the mesh UV map to the map of the texture
 		originalUV.CopyTo(transformUV, 0);
-			
 		for (int j = 0; j < objectMesh.uv.Length; j++) {
-				
 			transformUV[j].x = (transformUV[j].x * textureUV[counter].width) + textureUV[counter].x;
 			transformUV[j].y = (transformUV[j].y * textureUV[counter].height) + textureUV[counter].y;
 		}
-		
 		// Increase counter and set the object's mesh to the transformation.
-		
-		
 		if (!pause) {
-			
 			counter++;
 		}
-		
-		
 		objectMesh.uv = transformUV;
-		
 		yield return null;
 	}
-	
 	// Replaces Update() to allow for proper co-routine execution at runtime. Starts the animation co-routine.
-	
 	IEnumerator CoUpdate() {
-		
 		while (true) {
-		
 			if (counter >= textureUV.Length) {
-				
 				counter = 0;
 			}
-			
+			// Start the sprite animation co-routine
 			StartCoroutine("FrameAnimation");
-			
 			// Allows control of time in-between frame animations.
-			
 			yield return new WaitForSeconds((float)(1 / framesPerSecond));
 		}
 	}
-	
+	// Read UV map from file
 	Rect[] readUVMap() {
-		
 		// Intializes a reader and retrieves length of UV entries
-		
 		StringReader reader = new StringReader(uvMap.text);
 		int length = int.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
-		
 		Rect[] uvArray = new Rect[length];
-		
 		// Stores entries from text file into array and returns it
-		
 		for (int i = 0; i < length; i++) {
-			
 			float x = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
 			float y = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
 			float width = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
 			float height = float.Parse(reader.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
-			
 			uvArray[i] = new Rect(x, y, width, height);
 		}
-		
 		return uvArray;
 	}
 } 
